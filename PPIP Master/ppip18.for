26f03c6756ac7c73ad9789ddac0e202ebbe9f7f0
ontheslab <serverzaphod@gmail.com>
2026-03-16 10:31:16 +1100

    PPIP Master — original Z80 assembly source, D. Jewett III (1985-1988)

---
+++ PPIP Master/ppip18.for
@@ -0,0 +1,9 @@
+PPIP18 is a file copy utility that allows easy file copying between
+different drive/user areas.  Changes from PPIP173 concern support for
+Z80DOS type of timestamping.
+Library contains Z80 source, doc file, three .COM versions - one for
+ZCPR3 and Plu*Perfect Datestamper, another for plain CP/M,
+and the third for ZCPR3 and Z80DOS.  PPIP is fully 8080 compatable. 
+UNCR.COM and NULU or LSWEEP required.  
+
+
